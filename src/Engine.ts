22cb555f85df447a2ebb0767f810ec2224990992
Marcin Mrugas <marcin.mrugas@allegro.pl>
2022-10-25 16:53:32 +0200

    Improve circle collisions

--- src/Engine.ts
+++ src/Engine.ts
@@ -1,4 +1,4 @@
-import { Body, Circle, Polygon } from "./Body";
+import { Body, Circle } from "./Body";
 import { Vec2 } from "./Vector"
 import { Collision } from "./Collision";
 import { Solver } from "./Solver";
@@ -11,7 +11,7 @@ export class Engine {
     g: number;
     iterations: number;
 
-    constructor(debug = false, solver: Solver, iterations = 20) {
+    constructor(debug = false, solver: Solver, iterations = 35) {
         this.solver = solver;
         this.dt = 1 / 60; // take dt from elapsed time
         this.debug = debug;
@@ -40,7 +40,7 @@ export class Engine {
                 this.translationSolver();
             }
             for (let obj of this.gameObjects) {
-                obj.update(this.dt/ this.iterations);
+                obj.update(this.dt / this.iterations);
             }
         }
     };
@@ -63,29 +63,21 @@ export class Engine {
                 if (body1.isStatic && body2.isStatic) {
                     continue
                 }
-                if (!Collision.areColliding(body1, body2)) {
+                if (!Collision.areAABBColliding(body1, body2)) {
                     continue;
                 }
-                if (body1 instanceof Polygon) {
-                    if (body2 instanceof Polygon) {
-                        let mtv = Collision.calculateSAT(body1, body2);
-                        if (mtv !== false) {
-                            if (body2.isStatic) {
-                                body1.pos = body1.pos.add(mtv.normal.normalize().scale(-mtv.length))
-                            } else if (body1.isStatic) {
-                                body2.pos = body2.pos.add(mtv.normal.normalize().scale(mtv.length))
-                            } else {
-                                body2.pos = body2.pos.add(mtv.normal.normalize().scale(mtv.length / 2))
-                                body1.pos = body1.pos.add(mtv.normal.normalize().scale(-mtv.length / 2))
-                            }
-
-                            this.resolveCollision(body1, body2, mtv.normal)
-                        }
+                let collisionManifold = Collision.calculateSAT(body1, body2);
+                if (collisionManifold) {
+                    if (body2.isStatic) {
+                        body1.pos = body1.pos.add(collisionManifold.normal.normalize().scale(-collisionManifold.depth))
+                    } else if (body1.isStatic) {
+                        body2.pos = body2.pos.add(collisionManifold.normal.normalize().scale(collisionManifold.depth))
                     } else {
-                        throw new Error("Collision is only implemented between polygon and other polygon")
+                        body2.pos = body2.pos.add(collisionManifold.normal.normalize().scale(collisionManifold.depth / 2))
+                        body1.pos = body1.pos.add(collisionManifold.normal.normalize().scale(-collisionManifold.depth / 2))
                     }
-                } else {
-                    throw new Error("Collision with circle is not implemented yet")
+
+                    this.resolveCollision(body1, body2, collisionManifold.normal)
                 }
             }
         }
@@ -114,49 +106,50 @@ export class Engine {
             for (let j = i + 1; j < this.gameObjects.length; j++) {
                 let body1 = this.gameObjects[i]
                 let body2 = this.gameObjects[j]
-                if (!Collision.areColliding(body1, body2)) {
+                if (!Collision.areAABBColliding(body1, body2)) {
                     continue;
                 }
                 if (body1 instanceof Circle && body1 instanceof Circle) { //TODO add more cases
                     continue;
                 }
-                let mtv = Collision.calculateSAT(body1, body2);
-                if (mtv) {
-                    let incident = mtv.penetratingBody;
-                    let reference = mtv.referenceBody;
+                let collisionManifold
+                    = Collision.calculateSAT(body1, body2);
+                if (collisionManifold) {
+                    let incident = collisionManifold.body1;
+                    let reference = collisionManifold.body2;
                     // V + omega × r
-                    let penetratingVelocity = mtv.penetratingBody.v
-                        .add(mtv.penetratingPoint
+                    let penetratingVelocity = collisionManifold.body1.v
+                        .add(collisionManifold.contact1
                             .normal()
-                            .scale(mtv.penetratingBody.omega))
-                        .cross(mtv.normal);
-                    let referenceVelocity = mtv.referenceBody.v
-                        .add(mtv.referencePoint
+                            .scale(collisionManifold.body2.omega))
+                        .cross(collisionManifold.normal);
+                    let referenceVelocity = collisionManifold.body2.v
+                        .add(collisionManifold.body2.v
                             .normal()
-                            .scale(mtv.referenceBody.omega))
-                        .cross(mtv.normal);
+                            .scale(collisionManifold.body2.omega))
+                        .cross(collisionManifold.normal);
 
                     let relativeVelocity = penetratingVelocity - referenceVelocity;
                     let sign = Math.sign(relativeVelocity);
                     relativeVelocity = Math.abs(relativeVelocity);
 
-                    let rn1 = mtv.penetratingPoint.cross(mtv.normal);
-                    let rn2 = mtv.referencePoint.cross(mtv.normal);
+                    let rn1 = collisionManifold.contact1.cross(collisionManifold.normal);
+                    let rn2 = collisionManifold.contact2.cross(collisionManifold.normal);
 
-                    let k = mtv.penetratingBody.massInv + mtv.referenceBody.massInv;
-                    k += mtv.penetratingBody.inertiaInv * (rn1 * rn1);
-                    k += mtv.referenceBody.inertiaInv * (rn2 * rn2);
+                    let k = collisionManifold.body1.massInv + collisionManifold.body2.massInv;
+                    k += collisionManifold.body1.inertiaInv * (rn1 * rn1);
+                    k += collisionManifold.body2.inertiaInv * (rn2 * rn2);
 
                     let slop = 0.02;
-                    let bias = 0.2 / this.dt * Math.max(mtv.length - slop, 0);
+                    let bias = 0.2 / this.dt * Math.max(collisionManifold.depth - slop, 0);
                     let P = (relativeVelocity + sign * bias) / k;
 
-                    let Pvec = mtv.normal.scale(P).normal();
+                    let Pvec = collisionManifold.normal.scale(P).normal();
 
                     let refVector = Pvec.scale(sign);
                     let indVector = Pvec.scale(-1 * sign);
-                    incident.applyImpulse(indVector, mtv.penetratingPoint);
-                    reference.applyImpulse(refVector, mtv.penetratingPoint);
+                    incident.applyImpulse(indVector, collisionManifold.contact1);
+                    reference.applyImpulse(refVector, collisionManifold.contact2);
                 }
             }
         }
